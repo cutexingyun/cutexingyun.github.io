eedcf5dd0fcbc4da0a4f337ac8a0018c7b52a489
cutexingyun <hgw1234567hgw@163.com>
2024-05-18 12:04:24 +0800

    ain

--- Jspvz_xingyun/level/disappear.js
+++ Jspvz_xingyun/level/disappear.js
@@ -2,7 +2,7 @@
 alert('你被骗了');
 // 当用户被骗时，自动跳转到其他网站
 function redirectToOtherSite() {
-    window.location.href = 'https://player.bilibili.com/player.html?isOutside=true&aid=80433022&bvid=BV1GJ411x7h7&cid=137649199&p=1'; // 将URL替换为你想要跳转的网站
+    window.location.href = 'video/secret.html'; // 将URL替换为你想要跳转的网站
   }
  
   // 调用函数以触发跳转
