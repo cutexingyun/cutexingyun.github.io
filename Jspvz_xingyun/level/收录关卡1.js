d06745f2fb7bb3a91b40dc64b9c7e267bd8673eb
xingyun <164599903+cutexingyun@users.noreply.github.com>
2024-05-24 21:34:24 +0800

    更新 收录关卡1.js

--- Jspvz_xingyun/level/收录关卡1.js
+++ Jspvz_xingyun/level/收录关卡1.js
@@ -15,7 +15,7 @@ oS.Init({
     LoadMusic: "Mountains", // 加载音乐
     StartGameMusic: "Mountains", // 开始游戏音乐
     ArP: { // ArP对象
-        ArC: [1, 3], // ArC数组
+        ArC: [1, 5], // ArC数组
         ArR: [1, 5], // ArR数组
         Auto: 1, // 是否自动
         P: [
