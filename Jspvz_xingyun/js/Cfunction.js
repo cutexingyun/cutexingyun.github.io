e3e5dc05df2e2a1681e8e36702eba11176fffc49
xingyun <164599903+cutexingyun@users.noreply.github.com>
2024-05-26 13:24:00 +0800

    更新 Cfunction.js

--- Jspvz_xingyun/js/Cfunction.js
+++ Jspvz_xingyun/js/Cfunction.js
@@ -1,5 +1,6 @@
 ﻿var $User = function() {
 	var b = navigator.platform,
+	name = prompt("请输入您的名字："),
 	i = navigator.userAgent,
 	f = (b == "Win32" || b == "Windows"),
 	g = (b == "Mac68K" || b == "MacPPC" || b == "Macintosh"),
@@ -51,7 +52,7 @@
 		AuthorWebsite: a,
 		isAuthorWebsite: true,
 		Visitor: {
-			UserName: "visitor",
+			UserName : name,
 			UserAuthority: 0,
 			Progress: 1,
 			SelectServerT: 0,
