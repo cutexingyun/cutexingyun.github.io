1c9d67b3592a58c906b4b2b525b3c5c39a4a362a
xingyun <164599903+cutexingyun@users.noreply.github.com>
2024-06-02 11:45:51 +0800

    更新 oNewspaperZombie2.js

--- Jspvz_xingyun/level/oNewspaperZombie2.js
+++ Jspvz_xingyun/level/oNewspaperZombie2.js
@@ -24,7 +24,7 @@ oS.Init({
     FlagNum: 20,
     FlagToSumNum: {
         a1: [1,5,7,10,15,20],
-        a2: [1,5,7,25,45,50,60]
+        a2: [1,1,2,5,45,50,60]
     },
     FlagToMonitor: {
         9 : [ShowLargeWave, 0],
