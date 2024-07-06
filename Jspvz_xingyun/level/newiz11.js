9461989e17388242b06a4cfc7878bb69961334b5
xingyun <164599903+cutexingyun@users.noreply.github.com>
2024-07-06 19:32:57 +0800

    更新 newiz11.js

--- Jspvz_xingyun/level/newiz11.js
+++ Jspvz_xingyun/level/newiz11.js
@@ -1,6 +1,6 @@
 // 初始化游戏设置
 oS.Init( {
- PName:[oSplitPea,oGloomShroom,oRepeater2,oSpikerock,oSquash,oWallNut,oMagnetShroom,oCobCannon,oSunFlower,oStarFruit,oTorchwood,
+ PName:[oSplitPea,oGloomShroom,oRepeater2,oSpikerock,oSquash,oWallNut,oMagnetShroom,oCobCannon,oSunFlower,oStarfruit,oTorchwood,
 oPumpkinHead,oSpikeweed,oPuffShroom,oFlowerPot
     ], // 植物名称数组
     ZName: [oDiggerZombie,oImp],// 僵尸名称数组
